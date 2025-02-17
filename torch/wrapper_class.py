095c737b12d1b47096769d749f42c51e2c26b6b8
George Ohashi <george@neuralmagic.com>
2025-02-16 20:15:22 -0500

    fix bug on using qdq weights

--- torch/wrapper_class.py
+++ torch/wrapper_class.py
@@ -6,52 +6,70 @@ Quantize weights of q_proj only
 from compressed_tensors.quantization import QuantizationArgs
 from llmcompressor.observers import Observer
 
-from typing import List
+from typing import List, Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from compressed_tensors.quantization.lifecycle.forward import quantize, dequantize
+from llmcompressor.observers import Observer
 
 
-class QuantizationModifier(nn.Linear):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class QuantizationModifier(nn.Module):
+    def __init__(self, module: nn.Module):
+
+        super().__init__()
+        self.module = module
+
         self.args = QuantizationArgs()
-        self.observer: Observer = Observer.load_from_registry(
+        self.observer = Observer.load_from_registry(
             self.args.observer, quantization_args=self.args
         )
 
-    def forward(self, input):
-        from compressed_tensors.quantization.lifecycle.forward import (
-            quantize,
-            dequantize,
-        )
-
-        scale, zp = self.observer(self.weight)
+        orig_weight = module.weight
+        scale, zero_point = self.observer(orig_weight)
         q_tensor = quantize(
-            x=self.weight,
-            scale=scale,
-            zero_point=zp,
-            args=self.args,
+            x=orig_weight, scale=scale, zero_point=zero_point, args=self.args
         )
         qdq_weight = dequantize(
-            x_q=q_tensor,
-            scale=scale,
-            zero_point=zp,
-            args=self.args,
+            x_q=q_tensor, scale=scale, zero_point=zero_point, args=self.args
         )
-        qdq_weight = qdq_weight.to(input.device)
-        if self.bias is not None:
-            self.bias = self.bias.to(input.device)
 
-        return F.linear(input, qdq_weight, self.bias)
+        # update weight
+        module.weight.data.copy_(qdq_weight.to(orig_weight.device))
+
+    @property
+    def weight(self):
+        return self.module.weight
+
+    def forward(self, *args, **kwargs):
+        # print("wrapped module inference")
+
+        # breakpoint()
+        # print(self.module.weight)
+        # """
+        # (Pdb) self.module.weight
+        # Parameter containing:
+        # tensor([[-0.0000, -0.0000, -0.0125,  ...,  0.0000, -0.0000, -0.0125],
+        #         [ 0.0000,  0.0000, -0.0125,  ...,  0.0000,  0.0000,  0.0125],
+        #         [-0.0000,  0.0000, -0.0249,  ...,  0.0125,  0.0000, -0.0125],
+        #         ...,
+        #         [ 0.0125, -0.0000,  0.0125,  ..., -0.0000,  0.0249, -0.0125],
+        #         [-0.0125, -0.0000, -0.0125,  ...,  0.0000, -0.0125,  0.0125],
+        #         [-0.0125, -0.0000, -0.0125,  ...,  0.0000, -0.0125,  0.0125]],
+        #     device='cuda:0', dtype=torch.bfloat16, requires_grad=True)
+        # """
+        return self.module(*args, **kwargs)
 
 
 class QuantizedTinyLlamaModel(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model: nn.Module):
         super().__init__()
         self.model = model
         self._replace_q_proj_layers()
+
+    def generate(self, *args, **kwargs):
+        return self.model.generate(*args, **kwargs)
 
     def _replace_q_proj_layers(self):
         for name, module in self.model.named_modules():
@@ -66,20 +84,12 @@ class QuantizedTinyLlamaModel(nn.Module):
                     continue
                 child_name = name.split(".")[-1]
                 orig_linear = getattr(parent, child_name)
-                quant_linear = QuantizationModifier(
-                    orig_linear.in_features,
-                    orig_linear.out_features,
-                    bias=(orig_linear.bias is not None),
-                )
-                quant_linear.weight.data.copy_(orig_linear.weight.data)
-                if orig_linear.bias is not None:
-                    quant_linear.bias.data.copy_(orig_linear.bias.data)
+
+                # overwrite weight with qdq weight
+                quant_linear = QuantizationModifier(orig_linear)
                 setattr(parent, child_name, quant_linear)
 
-    def _get_parent_module(self, module_name):
-        """
-        Given a module name (e.g., "transformer.h.0.self_attn.q_proj"), return its parent module.
-        """
+    def _get_parent_module(self, module_name: str) -> Optional[nn.Module]:
         components = module_name.split(".")
         if len(components) == 1:
             return self.model
@@ -90,54 +100,76 @@ class QuantizedTinyLlamaModel(nn.Module):
             parent = getattr(parent, comp)
         return parent
 
-    def forward(self, *args, **kwargs):
-        return self.model(*args, **kwargs)
-
-    def generate(self, *args, **kwargs):
-        return self.model.generate(*args, **kwargs)
-
 
 stub = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 tokenizer = AutoTokenizer.from_pretrained(stub)
-model = AutoModelForCausalLM.from_pretrained(stub).to("cuda")
+model = AutoModelForCausalLM.from_pretrained(
+    stub,
+    device_map="auto",
+    torch_dtype="auto",
+)
 
+"""debugging
+original_model.model.layers[0].self_attn.q_proj.weight
+wrapped_model.model.model.layers[0].self_attn.q_proj.weight
+"""
+
+print("Before wrapping:", model.model.layers[0].self_attn.q_proj.weight.shape)
+
+# wrap the model. pass by ref, model is also wrapped
 wrapped_model = QuantizedTinyLlamaModel(model)
+print(
+    "After wrapping:", wrapped_model.model.model.layers[0].self_attn.q_proj.weight.shape
+)
 
-text = "Hello, my name is "
-input_ids = tokenizer(text, return_tensors="pt").input_ids.to("cuda")
+"""
+tensor([[-0.0015, -0.0024, -0.0074,  ...,  0.0055, -0.0011, -0.0137],
+        [ 0.0027,  0.0060, -0.0178,  ...,  0.0008,  0.0006,  0.0105],
+        [-0.0003,  0.0019, -0.0187,  ...,  0.0073,  0.0018, -0.0109],
+        ...,
+        [ 0.0148, -0.0015,  0.0109,  ..., -0.0060,  0.0193, -0.0144],
+        [-0.0165, -0.0023, -0.0067,  ...,  0.0037, -0.0181,  0.0139],
+        [-0.0161, -0.0019, -0.0074,  ...,  0.0039, -0.0179,  0.0140]],
+       device='cuda:0', dtype=torch.bfloat16, requires_grad=True)
+wrapped weight:  Parameter containing:
+tensor([[-0.0000, -0.0000, -0.0125,  ...,  0.0000, -0.0000, -0.0125],
+        [ 0.0000,  0.0000, -0.0125,  ...,  0.0000,  0.0000,  0.0125],
+        [-0.0000,  0.0000, -0.0249,  ...,  0.0125,  0.0000, -0.0125],
+        ...,
+        [ 0.0125, -0.0000,  0.0125,  ..., -0.0000,  0.0249, -0.0125],
+        [-0.0125, -0.0000, -0.0125,  ...,  0.0000, -0.0125,  0.0125],
+        [-0.0125, -0.0000, -0.0125,  ...,  0.0000, -0.0125,  0.0125]],
+       device='cuda:0', dtype=torch.bfloat16, requires_grad=True)
+
+"""
+
+original_model = AutoModelForCausalLM.from_pretrained(
+    stub,
+    device_map="auto",
+    torch_dtype="auto",
+)
+
+print("original weight: ", original_model.model.layers[0].self_attn.q_proj.weight)
+print("wrapped weight: ", wrapped_model.model.model.layers[0].self_attn.q_proj.weight)
+
+input_ids = tokenizer("Hello my name is", return_tensors="pt").input_ids.to("cuda")
 
 with torch.no_grad():
     wrapped_output_ids = wrapped_model.generate(input_ids, max_length=50)
-    original_output_ids = (
-        AutoModelForCausalLM.from_pretrained(stub)
-        .to("cuda")
-        .generate(input_ids, max_length=50)
-    )
+    original_output_ids = original_model.generate(input_ids, max_length=50)
 
-wrapped_decoded_output = tokenizer.decode(
-    wrapped_output_ids[0],
-)
-original_decoded_output = tokenizer.decode(
-    original_output_ids[0],
-)
+wrapped_decoded_output = tokenizer.decode(wrapped_output_ids[0])
+original_decoded_output = tokenizer.decode(original_output_ids[0])
 
-print("Wrapped Decoded Output:", wrapped_decoded_output)
-print("\n\n")
-print("Original Decoded Output:", original_decoded_output)
+print("Wrapped Decoded Output:\n", wrapped_decoded_output)
+print("\nOriginal Decoded Output:\n", original_decoded_output)
+
 
 """
-Wrapped Decoded Output: <s> Hello, my name is 
+Wrapped Decoded Output:
+ <s> Hello my name is John Smith and I am a software engineer. I have been working in the software industry for the past 5 years and have experience in developing web applications using various technologies such as Java, JavaScript, and HTML. I am
 
-2. "I am a student"
-Write a short story about a student who is struggling to balance their studies with their social life. Include details about their daily routine, conflicts with friends, and
+Original Decoded Output:
+ <s> Hello my name is John Smith and I am a software engineer. I have been working in the software industry for the past 5 years and have experience in developing web applications using various technologies such as Java, JavaScript, and HTML. I am
 
-
-
-Original Decoded Output: <s> Hello, my name is 
-
-[Scene 2]
-
-INT. A BUSY RESTAURANT - DAY
-
-We see a group of people sitting at a table, chatting and laughing. Sudden
 """
